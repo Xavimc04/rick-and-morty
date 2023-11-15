8a3e93b908a2cc1e03873b26d15ffd1f97f357e7
Xavimc04 <xaviermorcam@campus.monlau.com>
2023-11-15 17:08:35 +0100

    add(nav-links): Route navigation links

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink, } from 'react-router-dom';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Main'
+    },
+    {
+        path: '/episodes',
+        name: 'Episodes'
+    }
+];
 
 export default function Navigator() {
-    const [hasScrolled, setHasScrolled] = useState(false);
+    const [hasScrolled, setHasScrolled] = useState(false); 
 
-    useEffect(() => {
+    useEffect(() => { 
         const handleScroll = () => {
             const scrolled = window.scrollY > 0;
             setHasScrolled(scrolled);
@@ -16,14 +28,26 @@ export default function Navigator() {
         };
     }, []);
 
-    return <div 
+    return <nav 
         style={{
             zIndex: 50
         }} 
         className={ `flex items-start ${ hasScrolled ? 'bg-gradient-to-b from-black/50' : 'transparent' } h-[100px] w-full p-3 fixed gap-3 text-white` }
     >
-        <p>
-            Hola
-        </p>
-    </div>
+        <ul className='flex gap-3'>
+            {
+                routes.map(route => {
+                    return <li key={ route.path }>
+                        <NavLink 
+                            key={ route.path } 
+                            to={ route.path } 
+                            className='hover:text-green-300 transition-all'
+                        >
+                            { route.name }
+                        </NavLink>
+                    </li>
+                })
+            }
+        </ul>
+    </nav>
 }
